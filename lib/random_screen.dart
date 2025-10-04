b03311a3e9d581a9acc0a6bb92b27ed3cadf7f89
Matvey0808 <avto.mat.2000g@gmail.com>
2025-10-04 11:31:11 +0700

    Удалил все, начинаю заново

--- lib/random_screen.dart
+++ lib/random_screen.dart
@@ -1,61 +1,10 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:random_college/models/random_models.dart';
 
-class RandomScreen extends StatefulWidget {
-  const RandomScreen({super.key});
-
-  @override
-  State<RandomScreen> createState() => _RandomScreenState();
-}
-
-class _RandomScreenState extends State<RandomScreen> {
-  final List<Student> _students = [
-    Student(name: "Матвей", chance: 25.0, average: 4.0),
-    Student(name: "Игорь", chance: 20.0, average: 3.5),
-    Student(name: "Даниил", chance: 35.0, average: 5.0),
-    Student(name: "Алексей", chance: 20.0, average: 3.5),
-  ];
-
-  Student? _selectedStudent;
-  final Random _random = Random();
-  final List<Student> _selectedHistory = [];
-
-  Student _RandomStudents() {
-    double totalChance = _students.fold(
-      0,
-      (sum, student) => sum + student.chance,
-    );
-
-    double randomValue = _random.nextDouble() * totalChance;
-
-    double currentSum = 0;
-    for (var student in _students) {
-      currentSum += student.chance;
-      if (randomValue <= currentSum) {
-        return student;
-      }
-    }
-    return _students.last;
-  }
-
-    void _selectRandomStudent() {
-        setState(() {
-          _selectedStudent = _RandomStudents();
-          _selectedHistory.insert(0, _selectedStudent!);
-        });
-    }
-
-    void _resetSelection() {
-        setState(() {
-          _selectedStudent = null;
-          _selectedHistory.clear();
-        });
-    }
+class RandomScreen extends StatelessWidget {
+  const RandomScreen({super.key})
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+   return Scaffold();
   }
 }
